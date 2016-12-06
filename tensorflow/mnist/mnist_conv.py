05981a017dae53c8c4b3b768ad0c4d3f774e9832
Meng Li <jowett.lee@gmail.com>
2016-12-05 22:02:55 -0800

    Use validation set to verify during training process

--- tensorflow/mnist/mnist_conv.py
+++ tensorflow/mnist/mnist_conv.py
@@ -101,7 +101,7 @@ def main(_):
     batch = mnist.train.next_batch(50)
     if i % 100 == 0:
       train_accuracy = accuracy.eval(feed_dict={
-          x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
+          x: mnist.validation.images, y_: mnist.validation.labels, keep_prob: 1.0})
       print("step %d, training accuracy %g"%(i, train_accuracy))
     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
