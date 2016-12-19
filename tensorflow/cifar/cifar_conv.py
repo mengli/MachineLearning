a2c52fee2feae3dd1cdcb3f9bf364b26816db9d7
mengli <jowett.lee@gmail.com>
2016-12-19 09:37:25 -0800

    Adjust network

--- tensorflow/cifar/cifar_conv.py
+++ tensorflow/cifar/cifar_conv.py
@@ -31,8 +31,6 @@ def main(_):
     cifar10 = cifar.Cifar()
     cifar10.ReadDataSets(one_hot=True)
 
-    keep_prob = tf.placeholder(tf.float32)
-
     # Create the model
     x = tf.placeholder(tf.float32, [None, 3072])
 
@@ -42,48 +40,34 @@ def main(_):
     x_image = tf.reshape(x, [-1, 32, 32, 3])
 
     # (32 - 3 + 2 * 1) / 1 +1 = 32
-    W_conv1 = weight_variable([3, 3, 3, 32])
-    b_conv1 = bias_variable([32])
+    W_conv1 = weight_variable([5, 5, 3, 64])
+    b_conv1 = bias_variable([64])
 
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
-    W_conv2 = weight_variable([3, 3, 32, 64])
+    W_conv2 = weight_variable([5, 5, 64, 64])
     b_conv2 = bias_variable([64])
 
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
 
-    W_conv3 = weight_variable([3, 3, 64, 128])
-    b_conv3 = bias_variable([128])
+    W_fc1 = weight_variable([8 * 8 * 64, 384])
+    b_fc1 = bias_variable([384])
 
-    h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
-    h_pool3 = max_pool_2x2(h_conv3)
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 8 * 8 * 64])
 
-    W_fc1 = weight_variable([4 * 4 * 128, 1024])
-    b_fc1 = bias_variable([1024])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-    h_pool3_flat = tf.reshape(h_pool3, [-1, 4 * 4 * 128])
-
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)
-    #h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-    W_fc2 = weight_variable([1024, 1024])
-    b_fc2 = bias_variable([1024])
+    W_fc2 = weight_variable([384, 192])
+    b_fc2 = bias_variable([192])
 
     h_fc2 = tf.nn.relu(tf.matmul(h_fc1, W_fc2) + b_fc2)
-    #h_fc2_drop = tf.nn.dropout(h_fc2, keep_prob)
 
-    W_fc3 = weight_variable([1024, 512])
-    b_fc3 = bias_variable([512])
+    W_fc3 = weight_variable([192, 10])
+    b_fc3 = bias_variable([10])
 
-    h_fc3 = tf.nn.relu(tf.matmul(h_fc2, W_fc3) + b_fc3)
-    #h_fc3_drop = tf.nn.dropout(h_fc3, keep_prob)
-
-    W_fc4 = weight_variable([512, 10])
-    b_fc4 = bias_variable([10])
-
-    y_conv = tf.matmul(h_fc3, W_fc4) + b_fc4
+    y_conv = tf.matmul(h_fc2, W_fc3) + b_fc3
 
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
     train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
