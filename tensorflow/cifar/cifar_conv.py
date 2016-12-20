4c012b20c5743a4e68d95b5e9f17ecb7813f1972
mengli <jowett.lee@gmail.com>
2016-12-20 09:38:51 -0800

    fix issue that doesn't conv

--- tensorflow/cifar/cifar_conv.py
+++ tensorflow/cifar/cifar_conv.py
@@ -9,95 +9,95 @@ import tensorflow as tf
 
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
+   initial = tf.truncated_normal(shape, stddev=0.1)
+   return tf.Variable(initial)
 
 
 def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
+   initial = tf.constant(0.1, shape=shape)
+   return tf.Variable(initial)
 
 
 def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+   return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1], padding='SAME')
+   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                         strides=[1, 2, 2, 1], padding='SAME')
 
 
 def main(_):
-    cifar10 = cifar.Cifar()
-    cifar10.ReadDataSets(one_hot=True)
+   cifar10 = cifar.Cifar()
+   cifar10.ReadDataSets(one_hot=True)
 
-    # Create the model
-    x = tf.placeholder(tf.float32, [None, 3072])
+   # Create the model
+   x = tf.placeholder(tf.float32, [None, 3072])
 
-    # Define loss and optimizer
-    y_ = tf.placeholder(tf.float32, [None, 10])
+   # Define loss and optimizer
+   y_ = tf.placeholder(tf.float32, [None, 10])
 
-    x_image = tf.reshape(x, [-1, 32, 32, 3])
+   x_image = tf.reshape(x, [-1, 32, 32, 3])
 
-    # (32 - 3 + 2 * 1) / 1 +1 = 32
-    W_conv1 = weight_variable([5, 5, 3, 32])
-    b_conv1 = bias_variable([32])
+   # (32 - 3 + 2 * 1) / 1 +1 = 32
+   W_conv1 = weight_variable([5, 5, 3, 32])
+   b_conv1 = bias_variable([32])
 
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
+   h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+   h_pool1 = max_pool_2x2(h_conv1)
 
-    W_conv2 = weight_variable([5, 5, 32, 32])
-    b_conv2 = bias_variable([32])
+   W_conv2 = weight_variable([5, 5, 32, 64])
+   b_conv2 = bias_variable([64])
 
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    h_pool2 = max_pool_2x2(h_conv2)
+   h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+   h_pool2 = max_pool_2x2(h_conv2)
 
-    W_conv3 = weight_variable([5, 5, 32, 64])
-    b_conv3 = bias_variable([64])
+   W_conv3 = weight_variable([5, 5, 64, 64])
+   b_conv3 = bias_variable([64])
 
-    h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
+   h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
 
-    W_fc1 = weight_variable([8 * 8 * 64, 64])
-    b_fc1 = bias_variable([64])
+   W_fc1 = weight_variable([8 * 8 * 64, 384])
+   b_fc1 = bias_variable([384])
 
-    h_pool2_flat = tf.reshape(h_conv3, [-1, 8 * 8 * 64])
+   h_conv3_flat = tf.reshape(h_conv3, [-1, 8 * 8 * 64])
 
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+   h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
 
-    W_fc2 = weight_variable([64, 32])
-    b_fc2 = bias_variable([32])
+   W_fc2 = weight_variable([384, 192])
+   b_fc2 = bias_variable([192])
 
-    h_fc2 = tf.nn.relu(tf.matmul(h_fc1, W_fc2) + b_fc2)
+   h_fc2 = tf.nn.relu(tf.matmul(h_fc1, W_fc2) + b_fc2)
 
-    W_fc3 = weight_variable([32, 10])
-    b_fc3 = bias_variable([10])
+   W_fc3 = weight_variable([192, 10])
+   b_fc3 = bias_variable([10])
 
-    y_conv = tf.matmul(h_fc2, W_fc3) + b_fc3
+   y_conv = tf.matmul(h_fc2, W_fc3) + b_fc3
 
-    global_step = tf.Variable(0, trainable=False)
-    starter_learning_rate = 0.001
-    learning_rate = tf.train.exponential_decay(
-        starter_learning_rate, global_step, 100000, 0.1, staircase=True)
+   global_step = tf.Variable(0, trainable=False)
+   starter_learning_rate = 0.001
+   learning_rate = tf.train.exponential_decay(
+       starter_learning_rate, global_step, 100000, 0.1, staircase=True)
 
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
-    train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy, global_step=global_step)
-    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+   cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
+   train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy, global_step=global_step)
+   correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    sess = tf.InteractiveSession()
-    sess.run(tf.global_variables_initializer())
+   sess = tf.InteractiveSession()
+   sess.run(tf.global_variables_initializer())
 
-    for i in range(100000):
-        batch = cifar10.train.next_batch(128)
-        if i % 100 == 0:
-            train_accuracy = accuracy.eval(feed_dict={
-                x: cifar10.test.images, y_: cifar10.test.labels})
-            print("step %d, training accuracy %g" % (i, train_accuracy))
-        train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+   for i in range(100000):
+       batch = cifar10.train.next_batch(128)
+       if i % 100 == 0:
+           train_accuracy = accuracy.eval(feed_dict={
+               x: cifar10.test.images, y_: cifar10.test.labels})
+           print("step %d, training accuracy %g" % (i, train_accuracy))
+       train_step.run(feed_dict={x: batch[0], y_: batch[1]})
 
-    print("test accuracy %g" % accuracy.eval(feed_dict={
-        x: cifar10.test.images, y_: cifar10.test.labels}))
+   print("test accuracy %g" % accuracy.eval(feed_dict={
+       x: cifar10.test.images, y_: cifar10.test.labels}))
 
 
 if __name__ == '__main__':
-    tf.app.run(main=main)
+   tf.app.run(main=main)
