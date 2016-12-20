dc2d9a0872c3b781856a18ed708be8f5cc4613d1
mengli <jowett.lee@gmail.com>
2016-12-20 07:59:29 -0800

    Add conv layer

--- tensorflow/cifar/cifar_conv.py
+++ tensorflow/cifar/cifar_conv.py
@@ -40,31 +40,36 @@ def main(_):
     x_image = tf.reshape(x, [-1, 32, 32, 3])
 
     # (32 - 3 + 2 * 1) / 1 +1 = 32
-    W_conv1 = weight_variable([5, 5, 3, 64])
-    b_conv1 = bias_variable([64])
+    W_conv1 = weight_variable([5, 5, 3, 32])
+    b_conv1 = bias_variable([32])
 
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
-    W_conv2 = weight_variable([5, 5, 64, 64])
-    b_conv2 = bias_variable([64])
+    W_conv2 = weight_variable([5, 5, 32, 32])
+    b_conv2 = bias_variable([32])
 
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
 
-    W_fc1 = weight_variable([8 * 8 * 64, 384])
-    b_fc1 = bias_variable([384])
+    W_conv3 = weight_variable([5, 5, 32, 64])
+    b_conv3 = bias_variable([64])
 
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 8 * 8 * 64])
+    h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
+
+    W_fc1 = weight_variable([8 * 8 * 64, 64])
+    b_fc1 = bias_variable([64])
+
+    h_pool2_flat = tf.reshape(h_conv3, [-1, 8 * 8 * 64])
 
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-    W_fc2 = weight_variable([384, 192])
-    b_fc2 = bias_variable([192])
+    W_fc2 = weight_variable([64, 32])
+    b_fc2 = bias_variable([32])
 
     h_fc2 = tf.nn.relu(tf.matmul(h_fc1, W_fc2) + b_fc2)
 
-    W_fc3 = weight_variable([192, 10])
+    W_fc3 = weight_variable([32, 10])
     b_fc3 = bias_variable([10])
 
     y_conv = tf.matmul(h_fc2, W_fc3) + b_fc3
