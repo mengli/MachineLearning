05cbab4d456a7b7d5ee0fca2d853bec748882804
Meng Li <limeng@google.com>
2017-08-04 20:12:53 -0700

    Fix issue in evaluate

--- tensorflow/self_driving/segnet/evaluate.py
+++ tensorflow/self_driving/segnet/evaluate.py
@@ -8,8 +8,6 @@ import os
 import tensorflow as tf
 from utils import camvid
 from scipy import misc
-import matplotlib.cm as cm
-from matplotlib.colors import Normalize
 
 LOG_DIR = 'save'
 BATCH_SIZE = 4
@@ -21,11 +19,63 @@ NUM_CLASSES = 32
 
 test_dir = "/usr/local/google/home/limeng/Downloads/camvid/val.txt"
 
+colors = [
+    [64, 128, 64],  # Animal
+    [192, 0, 128],  # Archway
+    [0, 128, 192],  # Bicyclist
+    [0, 128, 64],  # Bridge
+    [128, 0, 0],  # Building
+    [64, 0, 128],  # Car
+    [64, 0, 192],  # CartLuggagePram
+    [192, 128, 64],  # Child
+    [192, 192, 128],  # Column_Pole
+    [64, 64, 128],  # Fence
+    [128, 0, 192],  # LaneMkgsDriv
+    [192, 0, 64],  # LaneMkgsNonDriv
+    [128, 128, 64],  # Misc_Text
+    [192, 0, 192],  # MotorcycleScooter
+    [128, 64, 64],  # OtherMoving
+    [64, 192, 128],  # ParkingBlock
+    [64, 64, 0],  # Pedestrian
+    [128, 64, 128],  # Road
+    [128, 128, 192],  # RoadShoulder
+    [0, 0, 192],  # Sidewalk
+    [192, 128, 128],  # SignSymbol
+    [128, 128, 128],  # Sky
+    [64, 128, 192],  # SUVPickupTruck
+    [0, 0, 64],  # TrafficCone
+    [0, 64, 64],  # TrafficLight
+    [192, 64, 128],  # Train
+    [128, 128, 0],  # Tree
+    [192, 128, 192],  # Truck_Bus
+    [64, 0, 64],  # Tunnel
+    [192, 192, 0],  # VegetationMisc
+    [0, 0, 0],  # Void
+    [64, 192, 0]  # Wall
+]
 
-def color_image(image, num_classes):
-    cmap = cm.get_cmap('Set1')
-    norm = Normalize(vmin=0., vmax=num_classes)
-    return cmap(norm(image))
+
+def color_mask(tensor, color):
+    return tf.reduce_all(tf.equal(tensor, color), 3)
+
+
+def one_hot(labels):
+    color_tensors = tf.unstack(colors)
+    channel_tensors = list(map(lambda color: color_mask(labels, color), color_tensors))
+    one_hot_labels = tf.cast(tf.stack(channel_tensors, 3), 'float32')
+    return one_hot_labels
+
+
+def rgb(logits):
+    softmax = tf.nn.softmax(logits)
+    argmax = tf.argmax(softmax, 3)
+    color_map = tf.constant(colors, dtype=tf.float32)
+    n = color_map.get_shape().as_list()[0]
+    one_hot = tf.one_hot(argmax, n, dtype=tf.float32)
+    one_hot_matrix = tf.reshape(one_hot, [-1, n])
+    rgb_matrix = tf.matmul(one_hot_matrix, color_map)
+    rgb_tensor = tf.reshape(rgb_matrix, [-1, IMAGE_HEIGHT, IMAGE_WIDTH, 3])
+    return tf.cast(rgb_tensor, tf.float32)
 
 
 def main(_):
@@ -63,11 +113,10 @@ def main(_):
                     train_label: label_batch,
                     is_training: True
                 }
-                prediction = sess.run([logits], feed_dict)
-                pred = tf.argmax(prediction[0], dimension=3).eval()
+                prediction = rgb(logits)
+                pred = sess.run([prediction], feed_dict)[0]
                 for batch in range(BATCH_SIZE):
-                    up_color = color_image(pred[batch], NUM_CLASSES)
-                    misc.imsave('output/segnet_camvid/decision_%d.png' % index, up_color)
+                    misc.imsave('output/segnet_camvid/decision_%d.png' % index, pred[batch])
                     misc.imsave('output/segnet_camvid/train_%d.png' % index, image_batch[batch])
                     index += 1
 
--- tensorflow/self_driving/segnet/evaluate_kitti.py
+++ tensorflow/self_driving/segnet/evaluate_kitti.py
@@ -8,10 +8,8 @@ import os
 import tensorflow as tf
 from utils import kitti_segnet
 from scipy import misc
-import matplotlib.cm as cm
-from matplotlib.colors import Normalize
 
-LOG_DIR = 'save'
+LOG_DIR = 'backup/segnet_kitti'
 EPOCH = 237
 BATCH_SIZE = 1
 IMAGE_HEIGHT = 375
@@ -20,11 +18,32 @@ NUM_CLASSES = 2
 
 test_dir = "/usr/local/google/home/limeng/Downloads/kitti/data_road/testing/test.txt"
 
+colors = [
+    [255, 0, 255],
+    [255, 0,   0],
+]
 
-def color_image(image, num_classes):
-    cmap = cm.get_cmap('Set1')
-    norm = Normalize(vmin=0., vmax=num_classes)
-    return cmap(norm(image))
+def color_mask(tensor, color):
+    return tf.reduce_all(tf.equal(tensor, color), 3)
+
+
+def one_hot(labels):
+    color_tensors = tf.unstack(colors)
+    channel_tensors = list(map(lambda color: color_mask(labels, color), color_tensors))
+    one_hot_labels = tf.cast(tf.stack(channel_tensors, 3), 'float32')
+    return one_hot_labels
+
+
+def rgb(logits):
+    softmax = tf.nn.softmax(logits)
+    argmax = tf.argmax(softmax, 3)
+    color_map = tf.constant(colors, dtype=tf.float32)
+    n = color_map.get_shape().as_list()[0]
+    one_hot = tf.one_hot(argmax, n, dtype=tf.float32)
+    one_hot_matrix = tf.reshape(one_hot, [-1, n])
+    rgb_matrix = tf.matmul(one_hot_matrix, color_map)
+    rgb_tensor = tf.reshape(rgb_matrix, [-1, IMAGE_HEIGHT, IMAGE_WIDTH, 3])
+    return tf.cast(rgb_tensor, tf.float32)
 
 
 def main(_):
@@ -60,13 +79,12 @@ def main(_):
                 feed_dict = {
                     train_data: image_batch,
                     train_label: label_batch,
-                    is_training: False
+                    is_training: True
                 }
-                prediction = sess.run([logits], feed_dict)
-                pred = tf.argmax(prediction[0], dimension=3).eval()
+                prediction = rgb(logits)
+                pred = sess.run([prediction], feed_dict)[0]
                 for batch in range(BATCH_SIZE):
-                    up_color = color_image(pred[batch], NUM_CLASSES)
-                    misc.imsave('output/segnet_kitti/decision_%d.png' % index, up_color)
+                    misc.imsave('output/segnet_kitti/decision_%d.png' % index, pred[batch])
                     misc.imsave('output/segnet_kitti/train_%d.png' % index, image_batch[batch])
                     index += 1
 
