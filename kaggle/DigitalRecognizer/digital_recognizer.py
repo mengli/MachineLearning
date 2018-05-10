ed1b2795dbe9c992093efb783fda0c569cca3f87
Meng Li <jowett.lee@gmail.com>
2018-05-09 21:34:00 -0700

    Improve model

--- kaggle/DigitalRecognizer/digital_recognizer.py
+++ kaggle/DigitalRecognizer/digital_recognizer.py
@@ -1,13 +1,6 @@
-'''Trains a simple convnet on the MNIST dataset.
-
-Gets to 99.25% test accuracy after 12 epochs
-(there is still a lot of margin for parameter tuning).
-16 seconds per epoch on a GRID K520 GPU.
-'''
-
 from __future__ import print_function
+import pandas as pd
 import keras
-from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
@@ -20,8 +13,16 @@ epochs = 12
 # input image dimensions
 img_rows, img_cols = 28, 28
 
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+train = pd.read_csv('C:\\Users\\jowet\\Downloads\\kaggle\\digit_recognizer\\train.csv')
+test = pd.read_csv('C:\\Users\\jowet\\Downloads\\kaggle\\digit_recognizer\\test.csv')
+
+x_train = train.drop(['label'], axis=1).as_matrix()
+y_train = train['label'].as_matrix()
+x_test = test.as_matrix()
+
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
 
 if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
@@ -42,7 +43,6 @@ print(x_test.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
@@ -63,8 +63,5 @@ model.compile(loss=keras.losses.categorical_crossentropy,
 model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+          verbose=1)
+result = model.predict(x_test, verbose=0)
--- kaggle/TalkingData/talking_data.py
+++ kaggle/TalkingData/talking_data.py
@@ -1,36 +1,136 @@
 import pandas as pd
+import time
 import numpy as np
 import lightgbm as lgb
-from sklearn.model_selection import train_test_split
+import gc
+import os
 
 
-def prepLGB(data, classCol='', IDCol='', fDrop=[]):
-    # Drop class column
-    if classCol != '':
-        labels = data[classCol]
-        fDrop = fDrop + [classCol]
-    else:
-        labels = None
-
-    if IDCol != '':
-        IDs = data[IDCol]
-    else:
-        IDs = None
-
-    if fDrop != []:
-        data = data.drop(fDrop, axis=1)
-
-    # Create LGB mats
-    lData = lgb.Dataset(data, label=labels, free_raw_data=False,
-                        feature_name=list(data.columns),
-                        categorical_feature='auto')
-
-    return lData, labels, IDs, data
+def do_count(df, group_cols, agg_name, agg_type='uint32', show_max=False, show_agg=True):
+    if show_agg:
+        print("Aggregating by ", group_cols, '...')
+    gp = df[group_cols][group_cols].groupby(group_cols).size().rename(agg_name).to_frame().reset_index()
+    df = df.merge(gp, on=group_cols, how='left')
+    del gp
+    if show_max:
+        print(agg_name + " max value = ", df[agg_name].max())
+    df[agg_name] = df[agg_name].astype(agg_type)
+    gc.collect()
+    return (df)
 
 
-if __name__ == '__main__':
-    train_columns = ['ip', 'app', 'device', 'os', 'channel', 'click_time', 'is_attributed']
-    test_columns = ['ip', 'app', 'device', 'os', 'channel', 'click_time', 'click_id']
+def do_countuniq(df, group_cols, counted, agg_name, agg_type='uint32', show_max=False, show_agg=True):
+    if show_agg:
+        print("Counting unqiue ", counted, " by ", group_cols, '...')
+    gp = df[group_cols + [counted]].groupby(group_cols)[counted].nunique().reset_index().rename(
+        columns={counted: agg_name})
+    df = df.merge(gp, on=group_cols, how='left')
+    del gp
+    if show_max:
+        print(agg_name + " max value = ", df[agg_name].max())
+    df[agg_name] = df[agg_name].astype(agg_type)
+    gc.collect()
+    return (df)
+
+
+def do_cumcount(df, group_cols, counted, agg_name, agg_type='uint32', show_max=False, show_agg=True):
+    if show_agg:
+        print("Cumulative count by ", group_cols, '...')
+    gp = df[group_cols + [counted]].groupby(group_cols)[counted].cumcount()
+    df[agg_name] = gp.values
+    del gp
+    if show_max:
+        print(agg_name + " max value = ", df[agg_name].max())
+    df[agg_name] = df[agg_name].astype(agg_type)
+    gc.collect()
+    return (df)
+
+
+def do_mean(df, group_cols, counted, agg_name, agg_type='float32', show_max=False, show_agg=True):
+    if show_agg:
+        print("Calculating mean of ", counted, " by ", group_cols, '...')
+    gp = df[group_cols + [counted]].groupby(group_cols)[counted].mean().reset_index().rename(
+        columns={counted: agg_name})
+    df = df.merge(gp, on=group_cols, how='left')
+    del gp
+    if show_max:
+        print(agg_name + " max value = ", df[agg_name].max())
+    df[agg_name] = df[agg_name].astype(agg_type)
+    gc.collect()
+    return (df)
+
+
+def do_var(df, group_cols, counted, agg_name, agg_type='float32', show_max=False, show_agg=True):
+    if show_agg:
+        print("Calculating variance of ", counted, " by ", group_cols, '...')
+    gp = df[group_cols + [counted]].groupby(group_cols)[counted].var().reset_index().rename(columns={counted: agg_name})
+    df = df.merge(gp, on=group_cols, how='left')
+    del gp
+    if show_max:
+        print(agg_name + " max value = ", df[agg_name].max())
+    df[agg_name] = df[agg_name].astype(agg_type)
+    gc.collect()
+    return (df)
+
+
+def lgb_modelfit_nocv(params, dtrain, dvalid, predictors, target='target', objective='binary', metrics='auc',
+                      feval=None, early_stopping_rounds=20, num_boost_round=3000, verbose_eval=10,
+                      categorical_features=None):
+    lgb_params = {
+        'boosting_type': 'gbdt',
+        'objective': objective,
+        'metric': metrics,
+        'learning_rate': 0.2,
+        'num_leaves': 31,  # we should let it be smaller than 2^(max_depth)
+        'max_depth': -1,  # -1 means no limit
+        'min_child_samples': 20,  # Minimum number of data need in a child(min_data_in_leaf)
+        'max_bin': 255,  # Number of bucketed bin for feature values
+        'subsample': 0.6,  # Subsample ratio of the training instance.
+        'subsample_freq': 0,  # frequence of subsample, <=0 means no enable
+        'colsample_bytree': 0.3,  # Subsample ratio of columns when constructing each tree.
+        'min_child_weight': 5,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
+        'subsample_for_bin': 200000,  # Number of samples for constructing bin
+        'min_split_gain': 0,  # lambda_l1, lambda_l2 and min_gain_to_split to regularization
+        'reg_alpha': 0,  # L1 regularization term on weights
+        'reg_lambda': 0,  # L2 regularization term on weights
+        'nthread': 4,
+        'verbose': 0,
+        'metric': metrics
+    }
+
+    lgb_params.update(params)
+
+    print("preparing validation datasets")
+
+    xgtrain = lgb.Dataset(dtrain[predictors].values, label=dtrain[target].values,
+                          feature_name=predictors,
+                          categorical_feature=categorical_features
+                          )
+    xgvalid = lgb.Dataset(dvalid[predictors].values, label=dvalid[target].values,
+                          feature_name=predictors,
+                          categorical_feature=categorical_features
+                          )
+
+    evals_results = {}
+
+    bst1 = lgb.train(lgb_params,
+                     xgtrain,
+                     valid_sets=[xgtrain, xgvalid],
+                     valid_names=['train', 'valid'],
+                     evals_result=evals_results,
+                     num_boost_round=num_boost_round,
+                     early_stopping_rounds=early_stopping_rounds,
+                     verbose_eval=10,
+                     feval=feval)
+
+    print("\nModel Report")
+    print("bst1.best_iteration: ", bst1.best_iteration)
+    print(metrics + ":", evals_results['valid'][metrics][bst1.best_iteration - 1])
+
+    return (bst1, bst1.best_iteration)
+
+
+def DO(frm, to, fileno):
     dtypes = {
         'ip': 'uint32',
         'app': 'uint16',
@@ -38,96 +138,172 @@ if __name__ == '__main__':
         'os': 'uint16',
         'channel': 'uint16',
         'is_attributed': 'uint8',
-        'click_id': 'uint32'
+        'click_id': 'uint32',
     }
 
-    train_data = pd.read_csv('C:\\Users\\jowet\\Downloads\\kaggle\\talking_data\\train_sample.csv',
-                             usecols=train_columns,
-                             dtype=dtypes)
-    train_data['click_datetime'] = pd.to_datetime(train_data['click_time'])
-    train_data['dow'] = train_data['click_datetime'].dt.dayofweek
-    train_data["doy"] = train_data["click_datetime"].dt.dayofyear
+    print('loading train data...', frm, to)
+    train_df = pd.read_csv("./train.csv", parse_dates=['click_time'], skiprows=range(1, frm), nrows=to - frm,
+                           dtype=dtypes,
+                           usecols=['ip', 'app', 'device', 'os', 'channel', 'click_time', 'is_attributed'])
 
-    test_data = pd.read_csv('C:\\Users\\jowet\\Downloads\\kaggle\\talking_data\\test_sample.csv', usecols=test_columns,
-                            dtype=dtypes)
-    test_data['click_datetime'] = pd.to_datetime(test_data['click_time'])
-    test_data['dow'] = test_data['click_datetime'].dt.dayofweek
-    test_data["doy"] = test_data["click_datetime"].dt.dayofyear
+    print('loading test data...')
+    test_df = pd.read_csv("./test.csv", parse_dates=['click_time'], dtype=dtypes,
+                          usecols=['ip', 'app', 'device', 'os', 'channel', 'click_time', 'click_id'])
 
-    testDataL, _, click_id, testData = prepLGB(test_data,
-                                               IDCol='click_id',
-                                               fDrop=['click_id', 'click_time', 'click_datetime'])
+    len_train = len(train_df)
+    train_df = train_df.append(test_df)
 
-    params = {'boosting_type': 'gbdt',
-              'max_depth': -1,
-              'objective': 'binary',
-              'nthread': 16,
-              'num_leaves': 64,
-              'learning_rate': 0.05,
-              'max_bin': 512,
-              'subsample_for_bin': 200,
-              'subsample': 1,
-              'subsample_freq': 1,
-              'colsample_bytree': 0.8,
-              'reg_alpha': 5,
-              'reg_lambda': 10,
-              'min_split_gain': 0.5,
-              'min_child_weight': 1,
-              'min_child_samples': 5,
-              'scale_pos_weight': 1,
-              'num_class': 1,
-              'metric': 'binary_error'}
+    del test_df
+    gc.collect()
 
-    mdl = lgb.LGBMClassifier(boosting_type='gbdt',
-                             objective='binary',
-                             n_jobs=5,
-                             silent=True,
-                             max_depth=params['max_depth'],
-                             max_bin=params['max_bin'],
-                             subsample_for_bin=params['subsample_for_bin'],
-                             subsample=params['subsample'],
-                             subsample_freq=params['subsample_freq'],
-                             min_split_gain=params['min_split_gain'],
-                             min_child_weight=params['min_child_weight'],
-                             min_child_samples=params['min_child_samples'],
-                             scale_pos_weight=params['scale_pos_weight'])
+    print('Extracting new features...')
+    train_df['hour'] = pd.to_datetime(train_df.click_time).dt.hour.astype('uint8')
+    train_df['day'] = pd.to_datetime(train_df.click_time).dt.day.astype('uint8')
 
-    print('Start training...')
-    # Kit k models with early-stopping on different training/validation splits
-    k = 12
-    predsValid = 0
-    predsTrain = 0
-    predsTest = 0
-    for i in range(0, k):
-        print('Fitting model %d' % i)
+    gc.collect()
+    train_df = do_countuniq(train_df, ['ip'], 'channel', 'X0', 'uint8', show_max=True)
+    gc.collect()
+    train_df = do_cumcount(train_df, ['ip', 'device', 'os'], 'app', 'X1', show_max=True)
+    gc.collect()
+    train_df = do_countuniq(train_df, ['ip', 'day'], 'hour', 'X2', 'uint8', show_max=True)
+    gc.collect()
+    train_df = do_countuniq(train_df, ['ip'], 'app', 'X3', 'uint8', show_max=True)
+    gc.collect()
+    train_df = do_countuniq(train_df, ['ip', 'app'], 'os', 'X4', 'uint8', show_max=True)
+    gc.collect()
+    train_df = do_countuniq(train_df, ['ip'], 'device', 'X5', 'uint16', show_max=True)
+    gc.collect()
+    train_df = do_countuniq(train_df, ['app'], 'channel', 'X6', show_max=True)
+    gc.collect()
+    train_df = do_cumcount(train_df, ['ip'], 'os', 'X7', show_max=True)
+    gc.collect()
+    train_df = do_countuniq(train_df, ['ip', 'device', 'os'], 'app', 'X8', show_max=True)
+    gc.collect()
+    train_df = do_count(train_df, ['ip', 'day', 'hour'], 'ip_tcount', show_max=True)
+    gc.collect()
+    train_df = do_count(train_df, ['ip', 'app'], 'ip_app_count', show_max=True)
+    gc.collect()
+    train_df = do_count(train_df, ['ip', 'app', 'os'], 'ip_app_os_count', 'uint16', show_max=True)
+    gc.collect()
+    train_df = do_var(train_df, ['ip', 'day', 'channel'], 'hour', 'ip_tchan_count', show_max=True)
+    gc.collect()
+    train_df = do_var(train_df, ['ip', 'app', 'os'], 'hour', 'ip_app_os_var', show_max=True)
+    gc.collect()
+    train_df = do_var(train_df, ['ip', 'app', 'channel'], 'day', 'ip_app_channel_var_day', show_max=True)
+    gc.collect()
+    train_df = do_mean(train_df, ['ip', 'app', 'channel'], 'hour', 'ip_app_channel_mean_hour', show_max=True)
+    gc.collect()
 
-        trainData, validData = train_test_split(train_data, test_size=0.4, stratify=train_data['is_attributed'])
-        trainDataL, trainLabels, trainIDs, trainData = prepLGB(trainData,
-                                                               classCol='is_attributed',
-                                                               fDrop=['click_time', 'click_datetime'])
-        validDataL, validLabels, validIDs, validData = prepLGB(validData,
-                                                               classCol='is_attributed',
-                                                               fDrop=['click_time', 'click_datetime'])
-        # Train
-        gbm = lgb.train(params,
-                        trainDataL,
-                        100000,
-                        valid_sets=[trainDataL, validDataL],
-                        early_stopping_rounds=50,
-                        verbose_eval=4)
+    print('doing nextClick')
+    predictors = []
 
-        # Predict
-        print('Start predicting using model %d' % i)
-        predsValid += gbm.predict(validData, num_iteration=gbm.best_iteration) / k
-        predsTrain += gbm.predict(trainData, num_iteration=gbm.best_iteration) / k
-        predsTest += gbm.predict(testData, num_iteration=gbm.best_iteration) / k
+    new_feature = 'nextClick'
+    filename = 'nextClick_%d_%d.csv' % (frm, to)
 
-        print('Save model %d' % i)
-        # save model k to file
-        gbm.save_model('model_%d.txt' % i)
+    if os.path.exists(filename):
+        print('loading from save file')
+        QQ = pd.read_csv(filename).values
+    else:
+        D = 2 ** 26
+        train_df['category'] = (train_df['ip'].astype(str) + "_" + train_df['app'].astype(str) + "_" + train_df[
+            'device'].astype(str) + "_" + train_df['os'].astype(str)).apply(hash) % D
+        click_buffer = np.full(D, 3000000000, dtype=np.uint32)
 
-    # Save submission
+        train_df['epochtime'] = train_df['click_time'].astype(np.int64) // 10 ** 9
+        next_clicks = []
+        for category, t in zip(reversed(train_df['category'].values), reversed(train_df['epochtime'].values)):
+            next_clicks.append(click_buffer[category] - t)
+            click_buffer[category] = t
+        del (click_buffer)
+        QQ = list(reversed(next_clicks))
+
+        print('saving')
+        pd.DataFrame(QQ).to_csv(filename, index=False)
+
+    train_df.drop(['epochtime', 'category', 'click_time'], axis=1, inplace=True)
+
+    train_df[new_feature] = pd.Series(QQ).astype('float32')
+    predictors.append(new_feature)
+
+    train_df[new_feature + '_shift'] = train_df[new_feature].shift(+1).values
+    predictors.append(new_feature + '_shift')
+
+    del QQ
+    gc.collect()
+
+    print("vars and data type: ")
+    train_df.info()
+    train_df['ip_tcount'] = train_df['ip_tcount'].astype('uint16')
+    train_df['ip_app_count'] = train_df['ip_app_count'].astype('uint16')
+    train_df['ip_app_os_count'] = train_df['ip_app_os_count'].astype('uint16')
+
+    target = 'is_attributed'
+    predictors.extend(['app', 'device', 'os', 'channel', 'hour', 'day',
+                       'ip_tcount', 'ip_tchan_count', 'ip_app_count',
+                       'ip_app_os_count', 'ip_app_os_var',
+                       'ip_app_channel_var_day', 'ip_app_channel_mean_hour',
+                       'X0', 'X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7', 'X8'])
+    categorical = ['app', 'device', 'os', 'channel', 'hour', 'day']
+    print('predictors', predictors)
+
+    test_df = train_df[len_train:]
+    val_df = train_df[(len_train - val_size):len_train]
+    train_df = train_df[:(len_train - val_size)]
+
+    print("train size: ", len(train_df))
+    print("valid size: ", len(val_df))
+    print("test size : ", len(test_df))
+
     sub = pd.DataFrame()
-    sub['click_id'] = click_id
-    sub['is_attributed'] = np.int32(predsTest > 0.5)
-    sub.to_csv('talking_data.csv', index=False)
+    sub['click_id'] = test_df['click_id'].astype('int')
+
+    gc.collect()
+
+    print("Training...")
+    start_time = time.time()
+
+    params = {
+        'learning_rate': 0.20,
+        'num_leaves': 7,  # 2^max_depth - 1
+        'max_depth': 3,  # -1 means no limit
+        'min_child_samples': 100,  # Minimum number of data need in a child(min_data_in_leaf)
+        'max_bin': 100,  # Number of bucketed bin for feature values
+        'subsample': 0.7,  # Subsample ratio of the training instance.
+        'subsample_freq': 1,  # frequence of subsample, <=0 means no enable
+        'colsample_bytree': 0.9,  # Subsample ratio of columns when constructing each tree.
+        'min_child_weight': 0,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
+        'scale_pos_weight': 200  # because training data is extremely unbalanced
+    }
+    (bst, best_iteration) = lgb_modelfit_nocv(params,
+                                              train_df,
+                                              val_df,
+                                              predictors,
+                                              target,
+                                              objective='binary',
+                                              metrics='auc',
+                                              early_stopping_rounds=30,
+                                              verbose_eval=True,
+                                              num_boost_round=1000,
+                                              categorical_features=categorical)
+
+    print('[{}]: model training time'.format(time.time() - start_time))
+    del train_df
+    del val_df
+    gc.collect()
+
+    print("Predicting...")
+    sub['is_attributed'] = bst.predict(test_df[predictors], num_iteration=best_iteration)
+    print("writing...")
+    sub.to_csv('sub_it%d.csv' % (fileno), index=False, float_format='%.9f')
+    print("done...")
+    return sub
+
+
+nrows = 184903891 - 1
+nchunk = 25000000
+val_size = 2500000
+
+frm = nrows - 75000000
+to = frm + nchunk
+
+sub = DO(frm, to, 0)
